e3a51d91c7de6752e2e032ef18d800a98eee999b
leexianhui <s10187343@connect.np.edu.sg>
2021-06-23 16:21:49 +0800

    current nuc setup

--- api/app.py
+++ api/app.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python
-
-from flask import Flask, Response
-from flask_cors import CORS
 import main
+from flask_cors import CORS
+from flask import Flask, Response
+import sys
+sys.path.append("/home/srtc/ENTER/lib/python3.7/site-packages")
 
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/camera')
 def stream():
     return Response(main.detect(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 if __name__ == '__main__':
-    app.run(debug=True, host = '192.168.31.2', port = 5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
