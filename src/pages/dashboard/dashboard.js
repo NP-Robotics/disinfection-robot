f9867da57e723396c969c3934e9010d28238a64d
leexianhui <leexianhui@live.com>
2021-08-02 09:28:10 +0800

    added voice when depth below 70 cm

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState} from "react";
 import "./dashboard.css";
 import Waypointfinder from "../waypointfinder/waypointfinder";
+import Voice from "./voice.wav";
 //import { propTypes } from "react-bootstrap/esm/Image";
 const ipAddress = global.ipAddress;
 
@@ -15,7 +16,10 @@ async function readDepth() {
 
 const Dashboard = (props) => {
   const [depth, setDepth] = useState([300]);
-  var intervalID = 0;
+  const [depth_trigger1, setDepth_trigger1] = useState(false)
+  const [depth_trigger2, setDepth_trigger2] = useState(false)
+  const [enabled, setEnabled] = useState(false)
+  var intervalID = 0
 
   useEffect(() => {
     async function fetchData() {
@@ -30,6 +34,37 @@ const Dashboard = (props) => {
       clearInterval(intervalID);
     };
   }, []);
+
+  useEffect(() => {
+    var smallest_depth= depth[0];
+    for (var i=0; i<=depth.length;i++){
+      if (depth[i]<smallest_depth) {
+        var smallest_depth=depth[i];
+      }
+    }
+
+    if (smallest_depth <= 70){
+      if (!depth_trigger1){
+        setDepth_trigger1(true)
+      }
+      else if(!depth_trigger2){
+        setDepth_trigger2(true)
+      }
+    }
+    else if(smallest_depth>70){
+      setDepth_trigger1(false)
+      setDepth_trigger2(false)
+      setEnabled(false)
+    }
+    console.log(depth_trigger2)
+    if(depth_trigger2 && !enabled){
+      console.log("hey")
+      let audio = new Audio(Voice);
+      audio.load();
+      audio.play();
+      setEnabled(true)
+    }
+  }, [depth])
 
   return (
     <div className="dashboard">
